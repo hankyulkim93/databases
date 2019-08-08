fe390861025297e39e500319588bb6738c2b70f7
hankyulkim93 <hankyulkim93@gmail.com>
2019-08-07 21:36:18 -0700

    need to work on server connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,20 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  // host: 'localhost',
+  // path: 3000,
+  user: 'root',
+  password: '',
+  database: 'chat'
+})
 
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
+connection.end()
